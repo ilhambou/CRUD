537a27290eb5f58ddbb845a30caaef065ccbb5a6
ilhambou <bouichilham2@gmail.com>
2022-08-05 11:49:58 +0200

    c1

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config({path: 'config.env'})
+const PORT = process.env.PORT || 8080
+
+app.get('/',(req,res)=>{
+    res.send('crud app');
+})
+
+app.listen(PORT,()=>{console.log(`Server is running on http://localhost:${PORT}`)});
