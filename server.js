b5d2ce4d89c8196fdddac9ec385e38cf0c499d97
ilhambou <bouichilham2@gmail.com>
2022-08-08 01:02:20 +0200

    c2

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 const app = express();
+const bodyparser = require("body-parser");
+const path = require('path')
 
 dotenv.config({path: 'config.env'})
 const PORT = process.env.PORT || 8080
 
+// log requests
+app.use(morgan("tiny"))
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({ extended:true }))
+
+//set view engine
+app.set("view engine","ejs")
+//app.set("views",path.resolve(__dirname,"views/ejs")) where is folder views
+
+//load assets 
+app.use('/css',express.static(path.resolve(__dirname,'assets/css')))
+app.use('/js',express.static(path.resolve(__dirname,'assets/js')))
+
+
 app.get('/',(req,res)=>{
-    res.send('crud app');
+    // res.send('crud app'); //return text
+    res.render('index');
+
+
 })
 
 app.listen(PORT,()=>{console.log(`Server is running on http://localhost:${PORT}`)});
