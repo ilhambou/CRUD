c85b241d0f754dcf7999b4d4cbabb677c277cbfd
ilhambou <bouichilham2@gmail.com>
2022-08-09 16:54:35 +0200

    c3 (mongodb)

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const app = express();
 const bodyparser = require("body-parser");
 const path = require('path')
 
+const connectDB = require('./server/database/connection')
+
 dotenv.config({path: 'config.env'})
 const PORT = process.env.PORT || 8080
 
 // log requests
 app.use(morgan("tiny"))
+
+// mongodb connection
+connectDB();
 
 //parse request to body-parser
 app.use(bodyparser.urlencoded({ extended:true }))
