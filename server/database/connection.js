1267f2ad283d64ddcb609932dfb0bad8e5dba43a
ilhambou <bouichilham2@gmail.com>
2022-08-12 09:57:57 +0200

    c4 (API)

--- server/database/connection.js
+++ server/database/connection.js
@@ -4,7 +4,6 @@ const connectDB = async () => {
     try{
         // mongodb connection string
         const con = await mongoose.connect(process.env.MONGO_URI, {
-          
         })
 
         console.log(`MongoDB connected : ${con.connection.host}`);
